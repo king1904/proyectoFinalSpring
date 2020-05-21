d7d429ed0ea82744835ddc4f7c27e065fdbf5751
Youcef <youcef1904@gmail.com>
2020-05-21 15:10:16 +0200

    changed the chat config

--- src/main/java/com/api/controllers/ChatController.java
+++ src/main/java/com/api/controllers/ChatController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.clases.ChatMessage;
+import com.api.clases.Usuario;
 
 @Controller
 @CrossOrigin("*")
@@ -21,6 +22,13 @@ import com.api.clases.ChatMessage;
 	@MessageMapping("/chat.register")
 	@SendTo("/topic/public")
 	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		Usuario user=new Usuario();
+		user.setUserDetails(chatMessage.getUser().getUserDetails());
+		user.setEmail(chatMessage.getUser().getEmail());
+		user.setUsername(chatMessage.getUser().getUsername());
+		
+		chatMessage.setUser(user);
+		
 		headerAccessor.getSessionAttributes().put("username", chatMessage.getUser().getUsername());
 		return chatMessage;
 	}
